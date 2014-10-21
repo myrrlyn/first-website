4aa5b93bb56fe4814c17bc7e5a506ab1371eab6b
myrrlyn <myrrlyn@outlook.com>
2014-10-21 04:07:00 -0400

    Construct basic layout and navigation JavaScript

--- resources/scripts/myrrlyn.js
+++ resources/scripts/myrrlyn.js
@@ -4,6 +4,176 @@
  * Powered by jQuery
  */
 
+//  Direct Execution
+$(document).ready(function ()
+{
+    //  Execute default page load
+    DebugPrint("Attempting initial pageload: " + SiteMap.Pages.DEFAULT[0]);
+    Navigate(SiteMap.Pages.DEFAULT);
+    //  Await user navigation
+    //  For some reason, jQueryNavigate cannot be defined in global scope and still
+    //  operate successfully.
+    $('a.myrr-page-loader').click(function jQueryNavigate()
+    {
+        /// <summary>
+        /// This method can only be called by a jQuery event, as it requires a valid 'this'
+        /// object. Specifically, it should only be called by elements that have both a
+        /// valid class (myrr-*-loader) and an id to identify the files to request.
+        /// </summary>
+        DebugPrint("CLICK EVENT");
+        var $ClickedID = this.id;
+        DebugPrint("ID of clicked element: " + $ClickedID);
+        $Selector = $('a#' + $ClickedID).attr('data-myrrPage');
+        DebugPrint("data-myrrPage of clicked element: " + $Selector);
+        $PageResult = NameResolve($Selector);
+        DebugPrint("SiteMap.Pages object: " + $PageResult);
+        Navigate($PageResult);
+    });
+});
+
+//#region Navigation
+
+//  Resource and display information
+var SiteMap =
+{
+    //  Directory of all pages to load
+    Bin: "/resources/documents/",
+    //  Elements that can request pageloads
+    Loaders:
+    [
+        "a.myrr-doc-loader",
+        "a.myrr-page-loader"
+    ],
+    //  List of file/function tuples for AJAX loading
+    Pages:
+    {
+        //  Default display page
+        DEFAULT: ["home", HomePage],
+        //  About Me
+        About: ["about", AboutMe],
+        //  Font display page
+        Fonts: ["fonts", FontsPage],
+        //  Frontpage
+        Home: ["home", HomePage],
+        //  Elder Scroll
+        KelJS: ["keljs", KelJS],
+        //  Collected Works
+        Oeuvre:
+        {
+            //  Index
+            Head:        ["oeuvre", IndexOeuvre],
+            Metaphysics: ["oeuvre/metaphysics", IndexMetaphysics],
+            Orcpocrypha: ["oeuvre/orcpocrypha", IndexOrcpocrypha],
+            Stories:     ["oeuvre/stories", IndexStories]
+        },
+        //  36 Lessons of Vivec
+        Sermons: ["36-lessons", Sermons]
+    },
+    //  Elements into which pages are loaded
+    Wrappers:
+    {
+        //  Article-specific block (main content)
+        Article: "div#myrr-article-wrapper",
+        //  Aside-specific block (sidebar)
+        Aside: "div#myrr-aside-wrapper",
+        //  Content block (holds both articles and asides)
+        Main:    "div#myrr-content-wrapper"
+    }
+};
+function Navigate($Page)
+{
+    /// <summary>
+    /// Performs AJAX loading of the specified pages into the main document.
+    /// </summary>
+    /// <param name="$Page" type="Array">
+    /// Tuple of ["name", callback()] containing the AJAX file and callback function.
+    /// </param>
+    $FilePath = SiteMap.Bin + $Page[0] + ".html ";
+    DebugPrint("File Path: " + $FilePath);
+    $Function = $Page[1];
+    DebugPrint("Function Call: " + $Function);
+    //  Pull article and aside elements from document
+    $Article  = $FilePath + "article";
+    $Aside    = $FilePath + "aside";
+    DebugPrint("Article: " + $Article);
+    DebugPrint("Aside:   " + $Aside);
+    //  Load the aside first, since it is smaller.
+    $(SiteMap.Wrappers.Aside).load($Aside, $Function());
+    //  Load the article second, since it is larger, and fire the $Callback script. The
+    //  callback will always be fired on the second AJAX load, regardless of the order
+    //  of text retrieval, since all the text needs to be loaded before the script can
+    //  reliably work.
+    $(SiteMap.Wrappers.Article).load($Article, $Function());
+    DebugPrint("Navigation attempted: " + $Page[0]);
+};
+function NameResolve($DataAttr)
+{
+    /// <summary>
+    /// Matches an input string (from the data-myrrPage element) against legal values
+    /// and returns a reference to the [file, function] tuple governing the AJAX
+    /// behavior of the specified document request.
+    /// </summary>
+    /// <param name="$DataAttr" type="String">
+    /// The raw string to be matched against elements of SiteMap.Pages. Typically
+    /// retrieved from the data-myrrPage attribute of a navigation element.
+    /// </param>
+    /// <returns type="Array">
+    /// ["filename", function()]
+    /// </returns>
+    var $Resolve
+    switch ($DataAttr)
+    {
+        case "About":
+            $Resolve = SiteMap.Pages.About;
+            break;
+        case "Fonts":
+            $Resolve = SiteMap.Pages.Fonts;
+            break;
+        case "Home":
+            $Resolve = SiteMap.Pages.Home;
+            break;
+        case "KelJS":
+            $Resolve = SiteMap.Pages.KelJS;
+            break;
+        case "Oeuvre":
+            $Resolve = SiteMap.Pages.Oeuvre.Head;
+            break;
+        case "Metaphysics":
+            $Resolve = SiteMap.Pages.Oeuvre.Metaphysics;
+            break;
+        case "Orcpocrypha":
+            $Resolve = SiteMap.Pages.Oeuvre.Orcpocrypha;
+            break;
+        case "Stories":
+            $Resolve = SiteMap.Pages.Oeuvre.Stories;
+            break;
+        case "Sermons":
+            $Resolve = SiteMap.Pages.Sermons;
+            break;
+        default:
+            $Resolve = SiteMap.Pages.DEFAULT;
+    }
+    return $Resolve;
+};
+//#endregion
+
+//#region Font Kit
+function FontsPage()
+{
+    DebugPrint("FontsPage callback executed");
+    alert("FontsPage callback execution!");
+}
+//#endregion
+
+function AboutMe() { DebugPrint("AboutMe function called"); }
+function HomePage() { DebugPrint("HomePage function called"); alert("HomePage function called!"); }
+function KelJS() { DebugPrint("KelJS function called"); }
+function IndexOeuvre() { DebugPrint("IndexOeuvre function called"); }
+function IndexMetaphysics() { DebugPrint("IndexOeuvre function called"); }
+function IndexOrcpocrypha() { DebugPrint("IndexOeuvre function called"); }
+function IndexStories() { DebugPrint("IndexOeuvre function called"); }
+function Sermons() { DebugPrint("36 Sermons function called"); }
+
 //#region Bugfixes
 
 //  IEMobile10 Viewport glitch
@@ -15,3 +185,17 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/))
 }
 
 //#endregion
+
+//  Toggles verbosity of debugging-specific console.log() calls
+var $DEBUGSTATUS = true;
+function DebugPrint($DEBUGINFO)
+{
+    /// <summary>
+    /// If global variable $DEBUGSTATUS is true, this prints to console whatever it is
+    /// passed. If $DEBUGSTATUS is false, it is mute.
+    /// </summary>
+    /// <param name="$DEBUGINFO">
+    /// The information to be logged to console for debugging purposes.
+    /// </param>
+    if ($DEBUGSTATUS) console.log($DEBUGINFO);
+}
