8c878afd5596d9a6e2d0f012be1be9f5beed70bf
myrrlyn <myrrlyn@outlook.com>
2014-10-26 02:54:52 -0400

    Add lore library and associated (loading only) functionality.
    TODO: Install scripts for modifying oeuvre texts.

---
+++ resources/scripts/oeuvre.js
@@ -0,0 +1,34 @@
+﻿/* === oeuvre.js ===
+ * Governs the library
+ * Written by myrrlyn
+ * Powered by jQuery
+ */
+
+//#region Loading
+
+function Oeuvre()
+{
+    DebugPrint("Oeuvre function called");
+    $(OeuvreIndex.Selector).click(function Loader()
+    {
+        var $Type = $(this).attr('data-type');
+        var $File = $(this).attr('data-file');
+        DebugPrint($Type + "/" + $File);
+        $(OeuvreIndex.Target).load(OeuvreIndex.Bin + $Type + "/" + $File + ".html");
+    });
+};
+
+var OeuvreIndex =
+{
+    Bin: "/resources/documents/oeuvre/",
+    Selector: "aside.oeuvre li",
+    Target: "article.oeuvre"
+};
+
+//#endregion
+
+//#region Sidebar
+
+
+
+//#endregion
