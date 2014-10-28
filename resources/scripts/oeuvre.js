9fe63e42cf5aef8dd216765c5e307fa8b43cdc1a
myrrlyn <myrrlyn@outlook.com>
2014-10-27 23:10:11 -0400

    Refactor oeuvre documents and start constructing their CSS

--- resources/scripts/oeuvre.js
+++ resources/scripts/oeuvre.js
@@ -9,13 +9,27 @@
 function Oeuvre()
 {
     DebugPrint("Oeuvre function called");
+    //  Handle index manipulation
+    $('aside.oeuvre h2.category').addClass('closed').click(function ()
+    {
+        $(this).addClass('active');
+        $(this).toggleClass('closed open');
+        $('aside.oeuvre h2.active + ol').slideToggle();
+        $(this).removeClass('active');
+    });
+    //  Handle click selections
     $(OeuvreIndex.Selector).click(function Loader()
     {
         var $Type = $(this).attr('data-type');
         var $File = $(this).attr('data-file');
         DebugPrint($Type + "/" + $File);
-        $(OeuvreIndex.Target).load(OeuvreIndex.Bin + $Type + "/" + $File + ".html");
+        OeuvreLoader($Type + "/" + $File + ".html");
     });
+};
+
+var OeuvreLoader = function ($Oeuvre)
+{
+    $(OeuvreIndex.Target).parent().load(OeuvreIndex.Bin + $Oeuvre);
 };
 
 var OeuvreIndex =
@@ -24,11 +38,5 @@ var OeuvreIndex =
     Selector: "aside.oeuvre li",
     Target: "article.oeuvre"
 };
-
-//#endregion
-
-//#region Sidebar
-
-
 
 //#endregion
